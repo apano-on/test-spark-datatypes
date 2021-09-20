43dd09f8ff2fc4b63f7d4c9f1d84a3d5840e770c
apano-on <73277915+apano-on@users.noreply.github.com>
2021-09-21 00:50:18 +0200

    Update start.sh

--- spark/start.sh
+++ spark/start.sh
@@ -11,6 +11,14 @@ start-worker.sh \
 	--port 7077 \
 	--webui-port 8051
 
+# Based on https://sedona.apache.org/tutorial/sql-pure-sql/
+# NOTE - Does not work because thriftserver does not seem to recognize this
+#spark-sql \
+#	--packages org.apache.sedona:sedona-python-adapter-3.0_2.12:1.0.1-incubating,org.apache.sedona:sedona-viz-3.0_2.12:1.0.1-incubating,org.datasyslab:geotools-wrapper:geotools-24.0 \
+#	--conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
+#	--conf spark.kryo.registrator=org.apache.sedona.viz.core.Serde.SedonaVizKryoRegistrator \
+#	--conf spark.sql.extensions=org.apache.sedona.viz.sql.SedonaVizExtensions,org.apache.sedona.sql.SedonaSqlExtensions
+
 spark-sql \
 	--name SPARKSQL-INSERTIONS \
 	--master spark://localhost:7077 \
